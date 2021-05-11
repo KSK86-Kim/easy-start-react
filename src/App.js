3de084a3b929561a02727a71e124f293677e4562
Oleksandr Polishchuk <70407461+KSK86-Kim@users.noreply.github.com>
2021-05-11 11:37:16 +0300

    start v1.2 fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Container from './components/Container';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -7,15 +7,20 @@ import Footer from './components/Footer';
 class App extends Component {
     render() {
         return (
-            <>
+            <Fragment>
                 <Header />
                 <Main>
-                    <Container>123</Container>
+                    <Container>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Aperiam nemo blanditiis neque quod modi ducimus
+                        eveniet, alias eius porro voluptas, qui quasi, ipsa
+                        tempora quaerat hic natus ratione ut incidunt?
+                    </Container>
                     <Container>123</Container>
                     <Container>123</Container>
                 </Main>
                 <Footer />
-            </>
+            </Fragment>
         );
     }
 }
